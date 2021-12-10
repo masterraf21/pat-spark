4ebd7d8721ebe01aa3796e5a7544c43dc6341dce
Rafi Abbel <masterraf21@gmail.com>
2021-12-10 19:45:16 +0700

    add something

--- src/main/scala/Follower.scala
+++ src/main/scala/Follower.scala
@@ -5,3 +5,36 @@ import org.apache.spark.graphx._
 import org.apache.spark.graphx.util.GraphGenerators 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+
+
+val conf = new SparkConf().setMaster("local[*").setAppName("pat")
+val sc = new SparkContext(conf)
+
+val file_path = "/home/masterraf21/Code/STI/pat-scala/data/tcx.txt"
+
+val graph = GraphLoader.edgeListFile(sc,file_path).reverse
+
+val nodeWithSuccessors: VertexRDD[Array[VertexId]] = graph.ops.collectNeighborIds(EdgeDirection.Out)
+
+val successorGraph = Graph(nodeWithSuccessors, graph.edges)
+
+val neighborNodes: VertexRDD[Set[VertexId]] = successorGraph.aggregateMessages[Set[VertexId]] (
+        triplet => {
+            Iterator((triplet.dstId, triplet.srcAttr.toSet))
+        },
+        (s1, s2) => s1 ++ s2
+    ).mapValues[Set[VertexId]](
+        (id: VertexId, neighbors: Set[VertexId]) => neighbors - id
+    )
+
+val neighborEdges = neighborNodes.flatMap[Edge[String]](
+  {
+    case (source: VertexId, allDests: Set[VertexId]) => {
+      allDests.map((dest: VertexId) => Edge(source, dest, ""))
+    }
+  }
+)
+
+val neighborGraph = Graph(graph.vertices, neighborEdges)
+
+neighborGraph.vertices.collect()
