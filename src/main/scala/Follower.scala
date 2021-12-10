d65bfeb0e7f81cf2071da029a737e084526463ae
Rafi Abbel <masterraf21@gmail.com>
2021-12-10 10:43:34 +0700

    init commit

---
+++ src/main/scala/Follower.scala
@@ -0,0 +1,7 @@
+import org.apache.spark._
+import org.apache.spark.rdd.RDD 
+import org.apache.spark.util.IntParam 
+import org.apache.spark.graphx._
+import org.apache.spark.graphx.util.GraphGenerators 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
